7703d4d49e336dbf0a7b14be175b96197cae6b52
tavernbiscuit <eclange12@gmail.com>
2024-07-24 08:07:33 -0500

    Added dict_as_list, and sort functions. Printed final steps in main function.

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ def main():
     
     words_counted = word_count(file_contents)
     chars_counted = count_chars(file_contents)
-    print(chars_counted)
+    dict_list = dict_as_list(chars_counted)
+    dict_list.sort(reverse=True, key=sort_on)
+
+
+    print("--- Begin report of books/frankenstein.txt ---\n")
+    print(f"{words_counted} found in the document\n")
+    
+    for entry in dict_list:
+        print(f"The '{entry['character']}' was found {entry['count']} times.")
+    print("\n--- End Report ---")
 
 def word_count(text):
     words = text.split()
@@ -15,7 +24,18 @@ def count_chars(text):
     string = text.lower()
     character_count = {}
     for char in string:
-        character_count[char] = character_count.get(char, 0) + 1
+        if char.isalpha():
+            character_count[char] = character_count.get(char, 0) + 1
     return character_count 
+
+def dict_as_list(dict):
+    char_list = []
+    for char, count in dict.items():
+        char_list.append({"character": char, "count": count})
+    return char_list
+
+def sort_on(list):
+    return list["count"]
+
 
 main()
